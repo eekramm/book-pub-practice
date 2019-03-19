591e122f0df60ff7169ce5709ca41f58f7693872
Eythan Kramm <kramm.eythan@gmail.com>
2019-03-19 13:11:26 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/bookpubpractice/BookPubPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.bookpubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPubPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPubPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/bookpubpractice/BookPubPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.bookpubpractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookPubPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
